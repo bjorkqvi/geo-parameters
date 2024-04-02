c8720b70936030ad96783e90c314705fa852dfa2
Jan-Victor Björkqvist <janvb@met.no>
2024-04-02 15:14:05 +0200

    imports

--- geo_parameters/__init__.py
+++ geo_parameters/__init__.py
@@ -1,4 +1,6 @@
-from .parameters import *
+from .parameters.wave import *
+from .parameters.ocean import *
+from .parameters.wind import *
 from .parameter_funcs import (
     list_of_parameters,
     dict_of_parameters,
