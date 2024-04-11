e12dc7c9862fc5cfa791fd4e26ff4e863ef18e11
Jan-Victor Björkqvist <janvb@met.no>
2024-04-11 14:54:49 +0200

    added x,y, and inds

--- geo_parameters/grid.py
+++ geo_parameters/grid.py
@@ -18,3 +18,27 @@ class Lat(MetaParameter):
     _long_name = "latitude"
     _standard_name = "latitude"
     _unit = "degrees_north"
+
+
+class X(MetaParameter):
+    name = "x"
+    _long_name = "x_distance"
+    _standard_name = "distance_in_x_direction"
+    _unit = ureg.m
+    _cf = False
+
+
+class Y(MetaParameter):
+    name = "y"
+    _long_name = "y_distance"
+    _standard_name = "distance_in_y_direction"
+    _unit = ureg.m
+    _cf = False
+
+
+class Inds(MetaParameter):
+    name = "inds"
+    _long_name = "index_of_points"
+    _standard_name = "index_of_geophysical_points"
+    _unit = "-"
+    _cf = False
