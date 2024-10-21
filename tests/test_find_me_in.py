fd962e6da1ccf5a8ce16f2a402c542b20b4f1e12
Jan-Victor Björkqvist <janvb@met.no>
2024-10-21 15:30:08 +0200

    fixed unit tests

--- tests/test_find_me_in.py
+++ tests/test_find_me_in.py
@@ -2,9 +2,12 @@ import geo_parameters as gp
 
 
 def test_docstring():
-    assert gp.wave.Hs.find_me_in([gp.wave.Hs, gp.wave.Tp]) == gp.wave.Hs
-    assert gp.wave.Hs.find_me_in([gp.wave.Dirp, gp.wave.Tp]) is None
-    assert gp.wave.Hs.find_me_in([]) is None
-    assert gp.wave.Hs.find_me_in(["hs"]) is None
+    assert gp.wave.Hs.find_me_in([gp.wave.Hs, gp.wave.Tp]) == [gp.wave.Hs]
+    assert (
+        gp.wave.Hs.find_me_in([gp.wave.Hs, gp.wave.Tp], return_first=True) == gp.wave.Hs
+    )
+    assert gp.wave.Hs.find_me_in([gp.wave.Dirp, gp.wave.Tp]) == []
+    assert gp.wave.Hs.find_me_in([], return_first=True) is None
+    assert gp.wave.Hs.find_me_in(["hs"]) == []
     hs = gp.wave.Hs("swh")
-    assert gp.wave.Hs.find_me_in([hs, "hs"]) == hs
+    assert gp.wave.Hs.find_me_in([hs, "hs"])[0] == hs
